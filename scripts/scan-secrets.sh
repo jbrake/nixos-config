46a3e95ff499ac4ab9066b76fdd0e46ec2c7520c
Jason Brake <pnut001@gmail.com>
2026-08-17 12:41:48 -0600

    Detect passwords declared in Nix

--- scripts/scan-secrets.sh
+++ scripts/scan-secrets.sh
@@ -7,7 +7,7 @@ repo_root="$(cd -- "$script_dir/.." && pwd)"
 cd "$repo_root"
 
 echo "Scanning Git history for secrets"
-gitleaks git --redact --no-banner .
+gitleaks git --config .gitleaks.toml --redact --no-banner .
 
 echo "Scanning the working tree for secrets"
-gitleaks dir --redact --no-banner .
+gitleaks dir --config .gitleaks.toml --redact --no-banner .
